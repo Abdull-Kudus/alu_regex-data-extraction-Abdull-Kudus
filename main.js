bb69352dc39878e23cb139d49e3d7b3b83fb48c8
Abdull-Kudus <a.zakariam@alustudent.com>
2025-09-17 00:31:15 +0200

    feat(regex): add 7 sample data object for testing

---
+++ main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Sample Object strings for each data type
+let str = {
+    "HTML tags" : ["<p>", '<div class="example">', '<img src="image.jpg" alt="description">'],
+    "Email addresses" : ["user@example.com", "fisrtname.lastname@company.co.uk"],
+    "Credit card numbers" : ["1234 5678 9012 3456", "1234-5678-9012-3456"],
+    "URLs" : ["https://www.example.com", "https://subdomain.example.org/page"],
+    "Phone Numbers" : ["(123) 456-7890", "123-456-7890", "123.456.7890"],
+    "Time" : ["14:30", "2:30 PM", "9:15 AM", "23:59", "12:00 PM"],
+    "Currency amounts" : ["$19.99", "$1,234.56", "$0.99", "$100"]
+
+};
